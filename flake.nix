8f10ed3766ae256098bbf5c7d0f9f9ee5f887e6e
Alaina <68250402+alaidriel@users.noreply.github.com>
2023-01-08 16:06:18 -0600

    chore: format nix with alejandra

--- flake.nix
+++ flake.nix
@@ -4,18 +4,21 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              nodejs-18_x
-              nodePackages.npm
-            ];
-          };
-        });
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs-18_x
+          nodePackages.npm
+        ];
+      };
+    });
 }
